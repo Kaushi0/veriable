01e5bc28f0c45787e8335e1ecd93f37dcb5fa371
Kaushi0 <109648858+Kaushi0@users.noreply.github.com>
2022-07-20 12:15:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,8 +54,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['94761532108'] //ur owner number
+global.ownername = "veriable 💜️ kandy2k" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
